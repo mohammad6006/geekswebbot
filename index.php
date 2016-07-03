5329af5fde81abd75288c2fce4c0a2df45a12f58
Mohammad <mohammad6006@gmail.com>
2016-07-03 20:45:36 +0430

    Update index.php

--- index.php
+++ index.php
@@ -85,10 +85,11 @@ try {
     }
     else
     {
+        $update->message->text = $mimik;
         $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
         $response = $client->sendMessage([
             'chat_id' => $update->message->chat->id,
-            'text' => "Invalid command, please use /help to get list of available commands"
+            'text' => $mimik " Invalid command, please use /help to get list of available commands"
             ]);
     }
 
