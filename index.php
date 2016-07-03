ca156f9d05aebc1df386591e2a8034c7024f11ff
Mohammad <mohammad6006@gmail.com>
2016-07-03 21:47:02 +0430

    Update index.php

--- index.php
+++ index.php
@@ -85,11 +85,11 @@ try {
     }
     else
     {
-        $update->message->text = $mimik;
+        $update->message->text = json_encode($mimik);
         $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
         $response = $client->sendMessage([
             'chat_id' => $update->message->chat->id,
-            'text' => $update->message->text
+            'text' => json_encode($update->message->text)
             ]);
     }
 
