e65c04850e7c3a8c4941a0c4f7751c6211458a2a
mohammad6006 <mohammad6006@gmail.com>
2016-07-13 19:28:09 +0430

    jul13

--- index.php
+++ index.php
@@ -154,8 +154,11 @@ try {
         //     'text' => $update->message->chat->id.'-'.$url
         // ]);
         $img = curl_file_create('https://d3k90kvix375hb.cloudfront.net/assets/home/hero/startup-10d700b2164d8d9ceb3934c15f01277c7a4bb2ce9d9c1d14d0bd00d680debafc.png','image/png');
-        $content = array('chat_id' => $update->message->chat->id, 'photo' => $img );
-        $response = $client->sendPhoto($content);
+        $response = $client->sendPhoto([
+            'chat_id'=> $update->message->chat->id,
+            'photo'=>$img,
+            'caption'=>'@TurkTv'
+            ]);
         // $response = $client->sendPhoto([
         //     'chat_id'=> $update->message->chat->id,
         //     'photo'=>file_get_contents('https://d3k90kvix375hb.cloudfront.net/assets/home/hero/startup-10d700b2164d8d9ceb3934c15f01277c7a4bb2ce9d9c1d14d0bd00d680debafc.png'),
