3eb9bef805c4d70516ce2836988acf4182e88020
mohammad6006 <mohammad6006@gmail.com>
2016-07-13 20:00:44 +0430

    jul13

--- index.php
+++ index.php
@@ -148,9 +148,10 @@ try {
         $img = curl_file_create('startup-10d700b2164d8d9ceb3934c15f01277c7a4bb2ce9d9c1d14d0bd00d680debafc.png','image/png');
         $response = $client->sendPhoto([
             'chat_id'=> $update->message->chat->id,
-            'photo'=>$img,
+            'photo'=>fopen('startup-10d700b2164d8d9ceb3934c15f01277c7a4bb2ce9d9c1d14d0bd00d680debafc.png','r'),
             'caption'=>'@TurkTv'
             ]);
+        print_r($response);
         // $ch = curl_init('https://d3k90kvix375hb.cloudfront.net/assets/home/hero/startup-10d700b2164d8d9ceb3934c15f01277c7a4bb2ce9d9c1d14d0bd00d680debafc.png');
         // $fp = fopen('flower.png', 'wb');
         // curl_setopt($ch, CURLOPT_FILE, $fp);
