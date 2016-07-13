adbc02963d9a8c4d8477f3c5f72de3d3a454b069
mohammad6006 <mohammad6006@gmail.com>
2016-07-13 11:24:20 +0430

    jul131

--- index.php
+++ index.php
@@ -106,11 +106,13 @@ try {
         ]);
     }
     elseif ($update->message->document) {
+        $response = $client->getFile();
         $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
         $response = $client->sendMessage([
             'chat_id' => $update->message->chat->id,
-            'text' => $update->message->document->file_id."ttt"
+            'text' => $update->message->chat->id.'-'.$update->message->document->file_id
         ]);
+
     }
     else
     {
