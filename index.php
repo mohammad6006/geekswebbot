6bcc3833b38c00f380efdbdec2f29296a6ad970f
mohammad6006 <mohammad6006@gmail.com>
2016-07-22 17:45:07 +0430

    jul22

--- index.php
+++ index.php
@@ -38,14 +38,14 @@ try {
     }
     elseif ($update->message->text == '/grab') {
         $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'upload_photo']);
-        $media = Bolandish\Instagram::getMediaByHashtag("wildlife", 2);
+        $media = Bolandish\Instagram::getMediaByHashtag("palang", 6);
         foreach($media as $value){
           if ($value->dimensions->width === $value->dimensions->height){
                 $url = trim(strtok($value->display_src, '?')); 
                 $response = $client->sendPhoto([
                     'chat_id'=> $update->message->chat->id,
                     'photo'=>fopen($url,'r'),
-                    'caption'=>'test'
+                    'caption'=>$value->caption
                     ]);
           }
         }
