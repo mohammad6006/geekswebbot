76044a70b0a6574c73cd895bc7ae8eaabe831598
mohammad6006 <mohammad6006@gmail.com>
2016-07-22 16:25:09 +0430

    jul22

---
+++ example.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+//Its getting 10 images/videoes from instagram with the hastag #nofilter
+$media = Bolandish\Instagram::getMediaByHashtag("nofilter", 10);
+
+//Its getting 10 images/videoes from Selena Gomezs instagram.. mmhh..
+$media = Bolandish\Instagram::getMediaByUserID(460563723, 10);
+
+//Get 10 images/videos after the media with the id 1060728019300790746 from Selena Gomex
+$media = Bolandish\Instagram::getMediaAfterByUserID(460563723,1060728019300790746, 10);
+
+//Display square images only
+$media = Bolandish\Instagram::getMediaByHashtag("wildlife", 20);
+foreach($media as $value){
+  if ($value->dimensions->width === $value->dimensions->height){
+    echo '<img src="' .$value->display_src. '" alt="' .$value->caption. '" />';
+  }
+}
+//Make images clickable and link to instagram post
+foreach($media as $key=>$value){
+  if ($value->dimensions->width === $value->dimensions->height){
+		echo '<a href="'.'https://www.instagram.com/p/'.$media[$key]->code.'" target="_blank"><img src="'.$media[$key]->display_src.'" alt="'.$media[$key]->caption.'" /></a>';
+  }
+}
