3d3431e65f5a06bd5ee082a46e735909a01dfbc7
Mohammad <mohammad6006@gmail.com>
2016-10-31 15:33:24 +0330

    parsing

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ try {
         $progtitles = $dom->find('div[class=ten columns]');
         $arri = '';
         foreach ($btimes as $key => $btime) {
-            $arri .= "*".$btime->text."*:<b>".$progtitles[$key]->text."</b> \n";
+            $arri .= $btime->text.":".$progtitles[$key]->text."\n";
         }
         $response = $client->sendMessage([
             'chat_id' => $update->message->chat->id,
