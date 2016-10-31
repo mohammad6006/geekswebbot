1fb588a12d4ac8856cfeb5a6dcc6f705f37d1780
Mohammad <mohammad6006@gmail.com>
2016-10-31 14:37:25 +0330

    parsing

--- index.php
+++ index.php
@@ -44,12 +44,13 @@ try {
         $btimes = $dom->find('div.two.columns.time');
         $progtitle = $dom->find('div.ten.columns');
         $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-        foreach ($btimes as $key => $btime) {
-            $response = $client->sendMessage([
-                'chat_id' => $update->message->chat->id,
-                'text' => $btime->text
-            ]);
-        }
+        // foreach ($btimes as $key => $btime) {
+
+        // }
+        $response = $client->sendMessage([
+            'chat_id' => $update->message->chat->id,
+            'text' => $btimes[0]->text
+        ]);
         // echo $a->text; // "click here"
     }
     // elseif ($update->message->text == '/vidiol') {
