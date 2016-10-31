33d0af1407441a697b21f778d0e4fdb495acb087
Mohammad <mohammad6006@gmail.com>
2016-10-31 13:35:42 +0330

    add phphtmlparser

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -60,6 +60,23 @@ class ComposerStaticInit95419506929a0927fedda1e6652c31ff
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        's' => 
+        array (
+            'stringEncode' => 
+            array (
+                0 => __DIR__ . '/..' . '/paquettg/string-encode/src',
+            ),
+        ),
+        'P' => 
+        array (
+            'PHPHtmlParser' => 
+            array (
+                0 => __DIR__ . '/..' . '/paquettg/php-html-parser/src',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Feed' => __DIR__ . '/..' . '/dg/rss-php/src/Feed.php',
         'FeedException' => __DIR__ . '/..' . '/dg/rss-php/src/Feed.php',
@@ -73,6 +90,7 @@ class ComposerStaticInit95419506929a0927fedda1e6652c31ff
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit95419506929a0927fedda1e6652c31ff::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit95419506929a0927fedda1e6652c31ff::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit95419506929a0927fedda1e6652c31ff::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit95419506929a0927fedda1e6652c31ff::$classMap;
 
         }, null, ClassLoader::class);
