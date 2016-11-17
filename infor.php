21551866b24d46901b76de8afbb5742cf4100d71
Mohammad <mohammad6006@gmail.com>
2016-11-17 17:25:35 +0330

    inforpage

--- infor.php
+++ infor.php
@@ -5,5 +5,5 @@ $token = getenv('acstok');
 $client = Zelenin\Telegram\Bot\ApiFactory::create($token);
 $update = json_decode(file_get_contents('php://input'));
 
-$response = $client->getUpdates;
+$response = $client->getUpdates();
 var_dump($response);
