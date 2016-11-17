20a62c068611e04570e2f7774b257ee108907eb9
Mohammad <mohammad6006@gmail.com>
2016-11-17 17:05:39 +0330

    inforpage

--- infor.php
+++ infor.php
@@ -4,5 +4,6 @@ require 'vendor/autoload.php';
 $token = getenv('acstok');
 $client = Zelenin\Telegram\Bot\ApiFactory::create($token);
 $update = json_decode(file_get_contents('php://input'));
-
-echo $update;
+echo "string";
+var_dump($client)
+var_dump($update)
