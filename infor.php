c36a9992f3a0d9d3bc7a33c4742803494278a9e3
Mohammad <mohammad6006@gmail.com>
2016-11-17 17:11:40 +0330

    inforpage

--- infor.php
+++ infor.php
@@ -4,6 +4,11 @@ require 'vendor/autoload.php';
 $token = getenv('acstok');
 $client = Zelenin\Telegram\Bot\ApiFactory::create($token);
 $update = json_decode(file_get_contents('php://input'));
-echo "string";
-var_dump($client);
-var_dump($update);
+
+
+try {
+    var_dump($update);
+    
+} catch (Zelenin\Telegram\Bot\Exception\NotOkException $e) {
+    echo $e->getMessage();
+}
