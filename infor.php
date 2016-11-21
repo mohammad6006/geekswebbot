4bbd2d13af5e1afdba4cf2fa24675f2f30bbe11d
Mohammad <mohammad6006@gmail.com>
2016-11-22 00:55:57 +0330

    infor

--- infor.php
+++ infor.php
@@ -9,11 +9,11 @@ $pw = $dbopts["pass"];
 $pdo = new PDO($dsn, $user, $pw);
 $fpdo = new FluentPDO($pdo);
 
+$query = $fpdo->from('messages')->where('id', 1);
+// $values = array('user_id' => '123', 'chat_id' => '321', 'message_id' => '456', 'daryaft' => 'abc', 'ersal' => 'def');       
+//     $query = $fpdo->insertInto('messages')->values($values);    
+//     $insert = $query->execute();
 
-$values = array('user_id' => '123', 'chat_id' => '321', 'message_id' => '456', 'daryaft' => 'abc', 'ersal' => 'def');       
-    $query = $fpdo->insertInto('messages')->values($values);    
-    $insert = $query->execute();
-
-    var_dump($insert);
+    var_dump($query);
     // will output:
     // string(1) "3"
