a80189b57972fb6d78ed9780a2449a4a8de98c8c
Mohammad <mohammad6006@gmail.com>
2016-11-21 15:28:52 +0330

    infor

--- infor.php
+++ infor.php
@@ -3,4 +3,6 @@ require 'vendor/autoload.php';
 
 $token = getenv('acstok');
 $dbopts = parse_url(getenv('DATABASE_URL'));
-echo $dbopts["user"];
+
+$pdo = new PDO('mysql:dbname='.ltrim($dbopts["path"],'/').', '.$dbopts["user"].', '.$dbopts["pass"]);
+$fpdo = new FluentPDO($pdo);
