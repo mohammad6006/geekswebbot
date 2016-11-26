048e5667a7d0c13697914972a20a8fa12cc11f0b
Mohammad <mohammad6006@gmail.com>
2016-11-26 15:31:31 +0330

    querytest

--- index.php
+++ index.php
@@ -233,7 +233,7 @@ try {
     else if($update->message->text == '/start')
     {
         $logger->addInfo('start:'.$update->message->message_id.'-'.$update->message->date.'-'.$update->message->text.'-'.$update->message->from->id.'-'.$update->message->from->first_name.'-'.$update->message->from->username);
-        $query = $fpdo->from('messages')->where('user_id = ?',$update->message->from->id);
+        $query = $fpdo->from('messages')->where('user_id = ?',$update->message->from->id)->execute();
         if (count($query)) {
            $logger->addInfo('megdar vojod darad');
         }else{
