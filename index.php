3ef0cef20605a8bda9c42af7075ce74f722a8b78
Mohammad <mohammad6006@gmail.com>
2016-11-27 15:44:05 +0330

    mp3test

--- index.php
+++ index.php
@@ -194,7 +194,7 @@ try {
         ]);
         $response = $client->sendAudio([
             'chat_id' => $update->message->chat->id,
-            'audio' => fopen($mpfile[1],'r'),
+            'audio' => $mpfile[1],
             'caption' => "Haydi Söyle \n Kalben \n @TurkTv",
             'performer' => '@TurkTv-Kalben',
             'title' => 'Haydi Söyle'
