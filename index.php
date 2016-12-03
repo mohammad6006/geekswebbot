dbb87aa3c8798d92db87fed20d83be27fc75f7c2
Mohammad <mohammad6006@gmail.com>
2016-12-03 18:54:10 +0330

    urlfunction

--- index.php
+++ index.php
@@ -131,10 +131,10 @@ try {
             ]);
     }
     elseif (isset($update->callback_query)) {
-        simpleTextSend($update->callback_query->message->chat->id,json_encode($update->callback_query));
         $dastor = $update->callback_query->data;
         if ($dastor == 'urltoinstapic') {
-            $query = $fpdo->from('messages')->where('user_id',$update->callback_query->from->id)->fetch();
+            simpleTextSend($update->callback_query->message->chat->id,json_encode($update->callback_query));
+            $query = $fpdo->from('messages')->where('user_id','66828424')->fetch();
             simpleTextSend($update->callback_query->message->chat->id,json_encode($query));
         }
         // $diziinsta = Bolandish\Instagram::getMediaByHashtag("karasevda", 2);
