6d3c47f0ce4885f81ee165582b7aa332c320f2ba
Mohammad <mohammad6006@gmail.com>
2016-12-07 23:30:34 +0330

    urlfunction

--- index.php
+++ index.php
@@ -182,13 +182,13 @@ try {
             $tem = $query[daryaft];
         $ssii = \Cloudinary\Uploader::upload($tem, array("resource_type" => "auto","timeout" => 60));
 
-            simpleTextSend($update->callback_query->message->chat->id,json_encode($ssii));
-        // $response = $client->sendChatAction(['chat_id' => $update->callback_query->message->chat->id, 'action' => 'upload_audio']);
-        // $response = $client->sendAudio([
-        //     'chat_id' => $update->callback_query->message->chat->id,
-        //     'audio' => 'https://res.cloudinary.com/drnd9jbicz/video/upload/v1480970950/pvi7edtgqci4jf2e2z0v.mp3'
-        //     ]);
-        //     simpleTextSend($update->callback_query->message->chat->id,json_encode($response));  
+            simpleTextSend($update->callback_query->message->chat->id,$ssii[secure_url]);
+            $response = $client->sendChatAction(['chat_id' => $update->callback_query->message->chat->id, 'action' => 'upload_audio']);
+            $response = $client->sendAudio([
+                'chat_id' => $update->callback_query->message->chat->id,
+                'audio' => $ssii[secure_url]
+                ]);
+                simpleTextSend($update->callback_query->message->chat->id,json_encode($response));  
         }
         // $diziinsta = Bolandish\Instagram::getMediaByHashtag("karasevda", 2);
         // Bolandish\Instagram::getMediaAfterByUserID(460563723, 1060728019300790746, 10);
