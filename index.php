cff10047dd9c32812bf83dcb6cab5346b0ce6c23
Mohammad <mohammad6006@gmail.com>
2017-01-08 16:00:49 +0330

    survivor

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ use PHPHtmlParser\Dom;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Abraham\TwitterOAuth\TwitterOAuth;
-define('CONSUMER_KEY', 'uA85crJv3zgAICaIcK9Cj4U5m');
-define('CONSUMER_SECRET', 'yIniiFc0Vq9qhXyswVP0iNeqAM9CVyRpDmM3yYJNvxfD48NBgA');
-define('ACCESS_TOKEN', '88899173-OMipak5KjBUXQu8i49G7dxOWt12Lv68bE8yccnGAn');
-define('ACCESS_TOKEN_SECRET', 'qbLTgO0dNs4Eixqzl983qbLf75UQgc5UK4tBbwqsQKpFv');
+define('CONSUMER_KEY', getenv('CONSUMER_KEY'));
+define('CONSUMER_SECRET', getenv('CONSUMER_SECRET'));
+define('ACCESS_TOKEN', getenv('ACCESS_TOKEN'));
+define('ACCESS_TOKEN_SECRET', getenv('ACCESS_TOKEN_SECRET'));
 
 
 $token = getenv('acstok');
