203dea8ba1d9dc01028848f013ef96bc00e5c328
Mohammad <mohammad6006@gmail.com>
2017-01-09 20:37:09 +0330

    survivor

--- infor.php
+++ infor.php
@@ -1,13 +1,11 @@
 <?php
 $survfile = file_get_contents("./survivor.json");
 $json_surv=json_decode($survfile);
-var_dump($json_surv);
-// print_r($json_surv);
-// $btns = [];
-// foreach($json_surv->unluler as $value) {
-//         echo $value->slug;
-//     }
-// print_r($btns);
+$btns = [];
+foreach($json_surv->unluler as $value) {
+     	   array_push($btns, [(array)$value]);
+    }
+print_r($btns);
 // require 'vendor/autoload.php';
 
 // $token = getenv('acstok');
