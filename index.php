e78a7fc5322eb50f4082708655d880bf65eeba43
Mohammad <mohammad6006@gmail.com>
2017-01-09 23:41:34 +0330

    survivor

--- index.php
+++ index.php
@@ -299,7 +299,7 @@ try {
                     $biofa = $value->biofa;
                 }
             }
-            $vvv = $name."\n".$image."\n".$biotr."\n".$biofa;
+            $vvv = $name."\n".$biotr."\n".$biofa.'<a href="'.$image.'"></a>';
             simpleTextSend($update->callback_query->message->chat->id,$vvv);
         }elseif (strpos(strtolower($dastor), 'program;') === 0) {
             $dizin = explode(';', $dastor);
