98cde509386db9d5738ef9e50153aa14b5ccd1c9
Mohammad <mohammad6006@gmail.com>
2017-01-20 18:24:07 +0330

    elogram

--- infor.php
+++ infor.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 use Larabros\Elogram\Client;
 $clientId = 'a44d4e709d97471f9c8c5d3112d8d81e';
 $clientSecret = '491b84904cac43529904997b2252ca1f';
-$redirectUrl = 'https://turktv.herokuapp.com';
+$redirectUrl = 'https://turktv.herokuapp.com/infor.php';
 
 $client = new Client($clientId, $clientSecret, null, $redirectUrl);
 
